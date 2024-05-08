382ac64dc8a88c1bef1ce7503b5210dd0f51f719
iajor <rjai.neto@gmail.com>
2024-05-08 20:06:44 -0300

    feat:views created

--- libraryApp/views.py
+++ libraryApp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from libraryApp.models import Author, Book
+from libraryApp.serializers import AuthorSerializer, BookSerializer
 
-# Create your views here.
+class AuthorListView(generics.ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
