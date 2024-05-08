ace19889ca4bc2494041583a36df4f4e3600aacb
iajor <rjai.neto@gmail.com>
2024-05-08 20:06:22 -0300

    feat:tests created

--- libraryApp/tests.py
+++ libraryApp/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from libraryApp.models import Author, Book
 
-# Create your tests here.
+class LibraryAPITest(TestCase):
+    def setUp(self):
+        self.author = Author.objects.create(name='Test Author')
+        self.book = Book.objects.create(name='Test Book', edition=1, publication_year=2022)
+        self.book.authors.add(self.author)
+
+    def test_author_list(self):
+        client = APIClient()
+        response = client.get('/authors/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_book_list(self):
+        client = APIClient()
+        response = client.get('/books/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_book_detail(self):
+        client = APIClient()
+        response = client.get(f'/books/{self.book.pk}/')
+        self.assertEqual(response.status_code, 200)
