407358180b540c9a0e5d28e0f8d974b8e69277c1
iajor <rjai.neto@gmail.com>
2024-05-08 21:42:55 -0300

    feat:command import created

---
+++ libraryApp/management/commands/import_author.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand
+from libraryApp.models import Author
+import csv
+
+
+class Command(BaseCommand):
+	help = 'create authors from a csv file'
+
+	def add_arguments(self, parser):
+		parser.add_argument('csv_path', type=str)
+		parser.add_argument('--hide', action='store_true', help='hide output print')
+
+	def handle(self, *args, **options):
+		csv_path = options['csv_path']
+
+		with open(csv_path, mode='r', encoding='utf-8') as csv_authors:
+			authors_reader = csv.reader(csv_authors)
+			next(authors_reader, None) #skip header
+
+			if not options['hide']:
+				self.stdout.write('importing authors from csv file...')
+
+			for name in authors_reader:
+				try:
+					new_author = Author(name=name[0])
+					new_author.save()
+				except Exception as e:
+					self.stdout.write(f'author: {name[0][:20]:20} - error: {e}')
+
+			if not options['hide']:
+				self.stdout.write('\n')
+				self.stdout.write('imported authors!')
