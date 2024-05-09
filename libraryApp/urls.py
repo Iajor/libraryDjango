09890767d7550566872763f9c918c4a24b149ecd
iajor <rjai.neto@gmail.com>
2024-05-08 21:57:21 -0300

    feat:urls updated

--- libraryApp/urls.py
+++ libraryApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from libraryApp.views import AuthorListView, BookListView, BookDetailView
+from libraryApp.views import *
 
 urlpatterns = [
     path('authors/', AuthorListView.as_view(), name='author-list'),
     path('books/', BookListView.as_view(), name='book-list'),
+    path('custom-books/', CustomBookListView.as_view(), name='custom-book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
 ]
